18a6cb8e6777280db92f1eca47b8684f646e9b77
Steven Pérez <stevenperez@Stevens-MacBook-Pro.local>
2016-03-22 11:42:57 -0600

    Initial config

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+	rename = require('gulp-rename'),
+	browserify = require('browserify'),
+	source = require('vinyl-source-stream');
+
+gulp.task('build-cli', () => {
+	return browserify({entries: './public/js/src/app.js', extensions: ['.js', '.jsx'], debug: true})
+		.transform('babelify', {presets: ['es2015', 'stage-3', 'react']})
+		.transform('brfs')
+		.bundle()
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest('./public/js/dist/'));
+});
+
+gulp.task('watch', () => {
+	gulp.watch([
+		'./public/js/src/**/*.js',
+		'./public/js/src/**/*.jsx'
+	], ['build-cli']);
+});
+
+gulp.task('default', ['watch']);
